6cb92417c1782334cd8f00fd99ebc69bceb076f0
Hitansh Agrawal <hitanshagrawal@gmail.com>
2024-03-14 17:45:27 +0530

    First Module Completed Finally

---
+++ app/code/Training/Example/Controller/Index/Index.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\Example\Controller\index;
+
+use Magento\Framework\App\ActionInterface;
+
+class Index implements ActionInterface{
+
+    public function execute(){
+        die("HEllo Hitansh");
+    }
+
+}
