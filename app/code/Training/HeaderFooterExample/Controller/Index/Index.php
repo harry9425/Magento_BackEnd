79fa4a174ef35e40d8e3a603fb8738d386c46c4f
Hitansh Agrawal <hitanshagrawal@gmail.com>
2024-03-15 14:53:03 +0530

    Added Head Scripts in storefront Magento

---
+++ app/code/Training/HeaderFooterExample/Controller/Index/Index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\HeaderFooterExample\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface{
+
+    protected $resultFactory;
+
+    public function __construct(PageFactory $resultFactory){
+        $this->resultFactory= $resultFactory;
+    }
+
+    public function execute(){
+        return $this->resultFactory->create();
+    }
+
+}
