3b69307b9a798e8ece23aaab1c15cd6dec0dff70
Hitansh Agrawal <hitanshagrawal@gmail.com>
2024-03-18 10:35:31 +0530

    Created a Cron Job which will run at every minute

---
+++ app/code/Training/CronExample/Cron/Example.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\CronExample\Cron;
+
+use Psr\Log\LoggerInterface;
+
+class Example{
+
+    protected LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $log)
+    {
+        $this->logger=$log;
+    }
+
+    public function execute(){
+        $this->logger->info('Starting running cron example');
+        sleep(2);
+        $this->logger->info('cron example finished');
+    }
+}
